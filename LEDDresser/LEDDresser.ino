c950ff0dd1273a43ac70b432a54a7f46e839257d
Steven Smethurst <steven@abluestar.com>
2016-03-02 22:53:54 -0800

    simple glow

--- LEDDresser/LEDDresser.ino
+++ LEDDresser/LEDDresser.ino
@@ -9,7 +9,7 @@
 #define NUM_LEDS_PER_STRIP  30
 #define NUM_TOTAL_LEDS      NUM_STRIPS *NUM_LEDS_PER_STRIP
 
-#define TIMER_STEP          100
+#define TIMER_STEP          20
 
 CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 
@@ -24,67 +24,30 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, 7>(leds[5], NUM_LEDS_PER_STRIP);
 }
 
-unsigned char hue = 0 ; 
-void loop() {  
-
-/*
-  // Set everthing to black. 
-  for(int x2 = 0; x2 < NUM_STRIPS; x2++) {
-    for(int y2 = 0; y2 < NUM_LEDS_PER_STRIP; y2++) {
-      leds[x2][y2] = CHSV(0, 0, 0);
-    }
-  }
-  FastLED.show();
-  delay(50);
-*/
-  
+ 
+unsigned char gHue = 0 ;
+void loop() 
+{ 
+  unsigned char hue = gHue ;
   
   // This outer loop will go over each strip, one at a time
   for(int x = 0; x < NUM_STRIPS; x++) {
-    // This inner loop will go over each led in the current strip, one at a time
-    for(int y = 0; y < NUM_LEDS_PER_STRIP; y++) {
-      leds[x][y] = CHSV(hue, 255, 255);
-      hue++; 
-      FastLED.show();
-
-  /*
-      // cycle the remainder LEDS 
-      int y2 = y+1;
-      for(int x2 = x; x2 < NUM_STRIPS; x2++) {
-        for(; y2 < NUM_LEDS_PER_STRIP; y2+=2) {
-          leds[x2][y2] = CHSV(hue, 0, 255); // Set to white 
-          FastLED.show();
-          leds[x2][y2] = CHSV(hue, 0, 0); // Set to black 
-        }
-        y2=0;
+    if( x % 2 == 0 ) { 
+      for(int y = 0; y < NUM_LEDS_PER_STRIP; y++) {
+        leds[x][y] = CHSV(hue, 255, 255);
+        hue++; 
+        // FastLED.show();      
       }
-      */
-    }
-
-    /*
-    // Flash the remaining panels. 
-    if( x < NUM_STRIPS-1 ) {
-      // Flash white 
-      for(int x2 = x+1; x2 < NUM_STRIPS; x2++) {
-        for(int y2 = 0; y2 < NUM_LEDS_PER_STRIP; y2++) {
-          leds[x2][y2] = CHSV(hue, 0, 255);
-        }
+    } else {
+      for(int y = NUM_LEDS_PER_STRIP; y > 0 ; y--) {
+        leds[x][y] = CHSV(hue, 255, 255);
+        hue++;
+        // FastLED.show();
       }
-      FastLED.show();
-      delay(50);
-      
-      // Set black 
-      for(int x2 = x+1; x2 < NUM_STRIPS; x2++) {
-        for(int y2 = 0; y2 < NUM_LEDS_PER_STRIP; y2++) {
-          leds[x2][y2] = CHSV(hue, 0, 0);
-        }
-      }
-      FastLED.show();
-      delay(50);
-    }
-    */
+    }    
   }
-
-  // delay(1000*3);
+  delay(TIMER_STEP);
+  gHue += 1 ;
+  FastLED.show();
   return ; 
 }
