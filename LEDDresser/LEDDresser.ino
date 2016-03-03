4345665f8a76c2dd6fd490f26c90610fe3cc4177
Steven Smethurst <steven@abluestar.com>
2016-03-02 21:44:22 -0800

    Hue

--- LEDDresser/LEDDresser.ino
+++ LEDDresser/LEDDresser.ino
@@ -5,10 +5,13 @@
 
 #include "FastLED.h"
 
-#define NUM_STRIPS 6
-#define NUM_LEDS_PER_STRIP 30
-CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
+#define NUM_STRIPS          6
+#define NUM_LEDS_PER_STRIP  30
+#define NUM_TOTAL_LEDS      NUM_STRIPS *NUM_LEDS_PER_STRIP
 
+#define TIMER_STEP          100
+
+CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 
 // For mirroring strips, all the "special" stuff happens just in setup.  We
 // just addLeds multiple times, once for each strip
@@ -21,47 +24,67 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, 7>(leds[5], NUM_LEDS_PER_STRIP);
 }
 
-void loop() {
-  /*
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[0][i] = CRGB::Red;
-  }
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[1][i] = CRGB::Green;
-  } 
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[2][i] = CRGB::Blue;
-  }
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[3][i] = CRGB::Yellow;
-  } 
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[4][i] = CRGB::Aqua;
-  }
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    leds[5][i] = CRGB::BlueViolet;
-  }
+unsigned char hue = 0 ; 
+void loop() {  
 
-    FastLED.show();
-
-return ; 
-  */
-  // This outer loop will go over each strip, one at a time
-  for(int x = 0; x < NUM_STRIPS; x++) {
-    // This inner loop will go over each led in the current strip, one at a time
-    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-      leds[x][i] = CRGB::Red;
-      FastLED.show();
-      delay(100);
-    }
-  }
-
-  // Set all to black 
-  for(int x = 0; x < NUM_STRIPS; x++) {
-    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-      leds[x][i] = CRGB::Black;
+/*
+  // Set everthing to black. 
+  for(int x2 = 0; x2 < NUM_STRIPS; x2++) {
+    for(int y2 = 0; y2 < NUM_LEDS_PER_STRIP; y2++) {
+      leds[x2][y2] = CHSV(0, 0, 0);
     }
   }
   FastLED.show();
-  delay(1000);
+  delay(50);
+*/
+  
+  
+  // This outer loop will go over each strip, one at a time
+  for(int x = 0; x < NUM_STRIPS; x++) {
+    // This inner loop will go over each led in the current strip, one at a time
+    for(int y = 0; y < NUM_LEDS_PER_STRIP; y++) {
+      leds[x][y] = CHSV(hue, 255, 255);
+      hue++; 
+      FastLED.show();
+
+  /*
+      // cycle the remainder LEDS 
+      int y2 = y+1;
+      for(int x2 = x; x2 < NUM_STRIPS; x2++) {
+        for(; y2 < NUM_LEDS_PER_STRIP; y2+=2) {
+          leds[x2][y2] = CHSV(hue, 0, 255); // Set to white 
+          FastLED.show();
+          leds[x2][y2] = CHSV(hue, 0, 0); // Set to black 
+        }
+        y2=0;
+      }
+      */
+    }
+
+    /*
+    // Flash the remaining panels. 
+    if( x < NUM_STRIPS-1 ) {
+      // Flash white 
+      for(int x2 = x+1; x2 < NUM_STRIPS; x2++) {
+        for(int y2 = 0; y2 < NUM_LEDS_PER_STRIP; y2++) {
+          leds[x2][y2] = CHSV(hue, 0, 255);
+        }
+      }
+      FastLED.show();
+      delay(50);
+      
+      // Set black 
+      for(int x2 = x+1; x2 < NUM_STRIPS; x2++) {
+        for(int y2 = 0; y2 < NUM_LEDS_PER_STRIP; y2++) {
+          leds[x2][y2] = CHSV(hue, 0, 0);
+        }
+      }
+      FastLED.show();
+      delay(50);
+    }
+    */
+  }
+
+  // delay(1000*3);
+  return ; 
 }
