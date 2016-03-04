909e3bbe1c6da8304e5fd6120cbce5cc8e8f4184
Steven Smethurst <steven@abluestar.com>
2016-03-03 21:57:20 -0800

    color cycle

--- LEDDresser/LEDDresser.ino
+++ LEDDresser/LEDDresser.ino
@@ -1,15 +1,10 @@
-// ArrayOfLedArrays - see https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples for more info on
-// using multiple controllers.  In this example, we're going to set up four NEOPIXEL strips on three
-// different pins, each strip getting its own CRGB array to be played with, only this time they're going
-// to be all parts of an array of arrays.
-
 #include "FastLED.h"
 
 #define NUM_STRIPS          6
 #define NUM_LEDS_PER_STRIP  30
 #define NUM_TOTAL_LEDS      NUM_STRIPS *NUM_LEDS_PER_STRIP
 
-#define TIMER_STEP          20
+#define TIMER_STEP          10
 
 CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 
@@ -26,28 +21,27 @@ void setup() {
 
  
 unsigned char gHue = 0 ;
+
 void loop() 
 { 
-  unsigned char hue = gHue ;
-  
-  // This outer loop will go over each strip, one at a time
-  for(int x = 0; x < NUM_STRIPS; x++) {
-    if( x % 2 == 0 ) { 
-      for(int y = 0; y < NUM_LEDS_PER_STRIP; y++) {
-        leds[x][y] = CHSV(hue, 255, 255);
-        hue++; 
-        // FastLED.show();      
+
+ for( unsigned char brightness = 80 ; brightness < 255 ; brightness ++ ) {
+    for( unsigned char count = 0 ; count < 20 ; count++ ) {
+      unsigned char hue = gHue ;
+      
+      // This outer loop will go over each strip, one at a time
+      for(int x = 0; x < NUM_STRIPS; x++) {
+        for(int y = 0; y < NUM_LEDS_PER_STRIP; y++) {
+          leds[x][y] = CHSV(hue, 255, brightness);
+          hue++;    
+        }
       }
-    } else {
-      for(int y = NUM_LEDS_PER_STRIP; y > 0 ; y--) {
-        leds[x][y] = CHSV(hue, 255, 255);
-        hue++;
-        // FastLED.show();
-      }
+        
+      gHue++ ;
+      FastLED.show();
+      delay(TIMER_STEP);
     }    
   }
-  delay(TIMER_STEP);
-  gHue += 1 ;
-  FastLED.show();
+    
   return ; 
 }
